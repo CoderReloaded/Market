07eb686efdb08487bcf8e3cf847ab0cf2a048903
CoderReloaded <116454966+CoderReloaded@users.noreply.github.com>
2023-03-26 23:02:11 +0200

    Update

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from typing import Union
 
-app = Flask(__name__)
+from fastapi import FastAPI
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+from pydantic import BaseModel
 
-@app.route('/about')
-def about():
-    return 'About'
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def index():
+    return {"version": "Space Market 1.0 beta."}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
